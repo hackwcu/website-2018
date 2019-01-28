57fe80e13b3a7b8a8ffa98f509a606e49e4587b8
Brandon Barker <contact@brandonbarker.me>
2019-01-28 16:30:12 -0500

    first commit

---
+++ wp-content/themes/hackwcu/js/scripts.js
@@ -0,0 +1,81 @@
+//window.onbeforeunload = function(){
+//	window.scrollTo(0,0);
+//}
+
+var $window = $(window),
+    $nav = $('.nav-section-1, .nav-section-2'),
+    $navtoggle = $('.nav-toggle'),
+    $button = $('.nav-toggle-button');
+
+function navtoggle() {
+    $button.on('click', function() {
+
+       $navtoggle.animate({
+           width:'toggle'},"fast"); 
+
+    });
+    
+     $window.on('resize', function() {
+     
+       if ($window.width() > 751) {
+           $navtoggle.css('display', 'none');
+       }
+ });
+
+}
+navtoggle();
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
